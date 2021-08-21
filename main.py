ec21fa411907ddfea0fbcc27b8ddea87c1e7161f
Adit Shetty <77937577+ashetty2308@users.noreply.github.com>
2021-08-20 22:32:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+
 
 medals_csv = pd.read_csv('medals.csv')
 print(medals_csv)
 
 
-#1
+# 1
 
-#2
 
-#3
+
+# 2
+
+# 3
 
 df = pd.DataFrame(np.random.rand(500, 5), columns =['A', 'B', 'C','D','E'])
 df['A'] = np.random.randint(1,1000, size=len(df))
@@ -20,5 +24,13 @@ df['E'] = np.random.randint(1,1000, size=len(df))
 
 df.to_csv('dataframe_csv_export.csv')
 
-print(df)
+# Summary Statistics
+# print(df.describe())
 
+print('Standard deviation: ', df.stack().std())
+
+new_df = df[df>(df.stack().std()*3)]
+new_df_csv = new_df.to_csv('new_df_csv2.csv')
+
+new_df = new_df.fillna(0.0)
+print(new_df)
